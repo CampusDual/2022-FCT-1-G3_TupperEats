783660cfffd93c705814c3426015d9747ea5c024
Santiago Xiao <santixiaorl@gmail.com>
2022-05-10 13:38:58 +0200

    creacion del servicio

---
+++ tuppereats-model/src/main/java/com/ontimize/tuppereats/model/core/service/MenuService.java
@@ -0,0 +1,41 @@
+package com.ontimize.tuppereats.model.core.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+import com.ontimize.tuppereats.api.core.service.IMenuService;
+import com.ontimize.tuppereats.model.core.dao.MenuDao;
+
+@Service
+@Lazy
+public class MenuService implements IMenuService{
+	@Autowired private MenuDao menuDao;
+	@Autowired private DefaultOntimizeDaoHelper daoHelper;
+	@Override
+	public EntityResult menuQuery(Map<String, Object> keyMap, List<String> attrList)
+			throws OntimizeJEERuntimeException {
+		 return this.daoHelper.query(this.menuDao, keyMap, attrList);
+	}
+	@Override
+	public EntityResult menuInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
+		return this.daoHelper.insert(this.menuDao, attrMap);
+	}
+	@Override
+	public EntityResult menuUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
+			throws OntimizeJEERuntimeException {
+		return this.daoHelper.update(this.menuDao, attrMap, keyMap);
+	}
+	@Override
+	public EntityResult menuDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
+		return this.daoHelper.delete(this.menuDao, keyMap);
+	}
+	
+	
+}
