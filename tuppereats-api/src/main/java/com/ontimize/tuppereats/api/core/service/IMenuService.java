15798b1243406e9059393a7bf61af33d05b71d25
Bitocg <jacobocastrogonzalez@gmail.com>
2022-05-10 13:31:50 +0200

    Interfaz de menu service

---
+++ tuppereats-api/src/main/java/com/ontimize/tuppereats/api/core/service/IMenuService.java
@@ -0,0 +1,21 @@
+package com.ontimize.tuppereats.api.core.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+
+public interface IMenuService {
+
+	// MENU
+		public EntityResult menuQuery(Map<String, Object> keyMap, List<String> attrList)
+				throws OntimizeJEERuntimeException;
+
+		public EntityResult menuInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException;
+
+		public EntityResult menuUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
+				throws OntimizeJEERuntimeException;
+
+		public EntityResult menuDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
+}
