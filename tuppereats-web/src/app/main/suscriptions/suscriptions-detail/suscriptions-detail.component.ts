41ae3b3a8f61629154c727a9be8b35052d9c624d
Bitocg <jacobocastrogonzalez@gmail.com>
2022-05-26 17:27:34 +0200

    detalles suscripciones

---
+++ tuppereats-web/src/app/main/suscriptions/suscriptions-detail/suscriptions-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-suscriptions-detail',
+  templateUrl: './suscriptions-detail.component.html',
+  styleUrls: ['./suscriptions-detail.component.css']
+})
+export class SuscriptionsDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
