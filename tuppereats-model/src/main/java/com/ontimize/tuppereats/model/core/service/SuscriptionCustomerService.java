4eac73e27142bd3ae8d73ce1b9852a5880d305cb
lopcarcar <carlos.lop.car95@gmail.com>
2022-05-31 12:19:29 +0200

    permisos

--- tuppereats-model/src/main/java/com/ontimize/tuppereats/model/core/service/SuscriptionCustomerService.java
+++ tuppereats-model/src/main/java/com/ontimize/tuppereats/model/core/service/SuscriptionCustomerService.java
@@ -5,10 +5,12 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+import com.ontimize.jee.common.security.PermissionsProviderSecured;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import com.ontimize.tuppereats.api.core.service.ISuscriptionCustomerService;
 import com.ontimize.tuppereats.model.core.dao.SuscriptionCustomerDao;
@@ -23,6 +25,7 @@ public class SuscriptionCustomerService implements ISuscriptionCustomerService {
     private DefaultOntimizeDaoHelper daoHelper;
 
     @Override
+	@Secured({ PermissionsProviderSecured.SECURED })
     public EntityResult suscriptionCustomerQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException {
         return this.daoHelper.query(this.suscriptionCustomerDao, keyMap, attrList);
     }
@@ -42,16 +45,19 @@ public class SuscriptionCustomerService implements ISuscriptionCustomerService {
 	}
 
     @Override
+	@Secured({ PermissionsProviderSecured.SECURED })
     public EntityResult suscriptionCustomerInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
         return this.daoHelper.insert(this.suscriptionCustomerDao, attrMap);
     }
 
     @Override
+	@Secured({ PermissionsProviderSecured.SECURED })
     public EntityResult suscriptionCustomerUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
         return this.daoHelper.update(this.suscriptionCustomerDao, attrMap, keyMap);
     }
 
     @Override
+	@Secured({ PermissionsProviderSecured.SECURED })
     public EntityResult suscriptionCustomerDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
         return this.daoHelper.delete(this.suscriptionCustomerDao, keyMap);
     }
