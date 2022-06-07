7b96386f76fbf20f592fdc14b163f7b84e770d24
lopcarcar <carlos.lop.car95@gmail.com>
2022-06-07 17:59:50 +0200

    Password oculta

--- tuppereats-web/src/app/shared/app.services.config.ts
+++ tuppereats-web/src/app/shared/app.services.config.ts
@@ -11,9 +11,6 @@ export const SERVICE_CONFIG: Object = {
   'masterproducts': {
     'path': '/masterproducts'
   },
-  'customers': {
-    'path': '/customers'
-  },
   'suscriptionCustomers': {
     'path': '/suscriptionCustomers'
   },
