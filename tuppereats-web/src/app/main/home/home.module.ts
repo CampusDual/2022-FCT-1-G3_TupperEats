91ed23e4cf814404674cfaae6df6345da28d91c0
Patricia Maceiras <patricia.maceiras@campusdual.com>
2022-06-08 16:33:24 +0200

    Añadimos el módulo de gráficas al módulo de home y añadimos una gráfica tipo tarta al home-html

--- tuppereats-web/src/app/main/home/home.module.ts
+++ tuppereats-web/src/app/main/home/home.module.ts
@@ -4,13 +4,14 @@ import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { SharedModule } from '../../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { OChartModule } from 'ontimize-web-ngx-charts';
 
 @NgModule({
   imports: [
     SharedModule,
     OntimizeWebModule,
-    HomeRoutingModule
-
+    HomeRoutingModule,
+    OChartModule
   ],
   declarations: [
     HomeComponent
