2fbc8d7de5c1fcbbef610ca40fb455473e44330c
lopcarcar <carlos.lop.car95@gmail.com>
2022-06-20 15:44:53 +0200

    Mi área de cliente

--- tuppereats-client-web/src/app/main/client-history/client-history.module.ts
+++ tuppereats-client-web/src/app/main/client-history/client-history.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { OntimizeWebModule } from 'ontimize-web-ngx';
+
 import { ClientHistoryRoutingModule } from './client-history-routing.module';
 import { ClientHistoryComponent } from './client-history.component';
 
@@ -9,6 +11,7 @@ import { ClientHistoryComponent } from './client-history.component';
   declarations: [ClientHistoryComponent],
   imports: [
     CommonModule,
+    OntimizeWebModule,
     ClientHistoryRoutingModule
   ]
 })
---
+++ tuppereats-client-web/src/app/shared/services/tupperEats.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, Injector } from "@angular/core";
+import { AuthService, OntimizeEEService, SessionInfo } from "ontimize-web-ngx";
+
+@Injectable()
+export class TupperEatsService extends OntimizeEEService{
+
+  sessionInfo:SessionInfo ;
+
+  constructor(protected injector:Injector){
+    super(injector);
+    this.sessionInfo = this.authService.getSessionInfo()
+  }
+
+  clientUser(kv?: object, av?: string[], entity?: string, sqltypes?: object){
+    kv["user_"] = this.sessionInfo.user
+    return this.query(kv,av, entity,sqltypes)
+  }
+}
