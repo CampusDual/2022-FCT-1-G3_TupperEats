68a690d31df6e8db1e33977496cc2b4e7f2e37ba
Santiago Xiao <santixiaorl@gmail.com>
2022-06-20 17:16:10 +0200

    empezamos parte de modificacion de datos cliente

--- tuppereats-client-web/src/app/main/client-personal-data/client-personal-data.module.ts
+++ tuppereats-client-web/src/app/main/client-personal-data/client-personal-data.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { OntimizeWebModule } from 'ontimize-web-ngx';
+
 import { ClientPersonalDataRoutingModule } from './client-personal-data-routing.module';
 import { ClientPersonalDataComponent } from './client-personal-data.component';
 
@@ -9,6 +11,7 @@ import { ClientPersonalDataComponent } from './client-personal-data.component';
   declarations: [ClientPersonalDataComponent],
   imports: [
     CommonModule,
+    OntimizeWebModule,
     ClientPersonalDataRoutingModule
   ]
 })
